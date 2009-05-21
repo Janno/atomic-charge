7d1021ebeefddf4d63177d7ef2baba7ca9b2a269
Janno <mail@janno-kaiser.de>
2009-05-21 23:18:21 +0200

    defect message length?

--- atomic-charge.py
+++ atomic-charge.py
@@ -2,15 +2,22 @@
 import bencode
 from hashlib import sha1
 from socket import socket
+from string import printable
 import struct
 import math
+import random
 
 def gen_handshake(meta):
         infohash = sha1(bencode.bencode(meta['info'])).digest()
         return chr(19) + 'BitTorrent protocol' + chr(0)*8 + infohash
 
 def gen_bitfield(meta):
-        num = int(math.ceil(len(meta['info']['pieces']) / 20 // 8))
+        num = len(meta['info']['pieces']) / 20 // 8
+##        result = ""
+##        for x in reversed(xrange(int(math.ceil(math.log(num,2))))):
+##                result = result + chr(num / (2**x))
+##                num = num % (2**x)
+##        return result
         return '\xFF'*num
 
 def gen_message(msgid, msg):
@@ -43,14 +50,20 @@ if __name__ == '__main__':
                 else:
                         print "gibberish, exiting!"
                         exit()
+        id = ''.join(random.choice(printable) for x in xrange(12))
+        sock.sendall(gen_handshake(meta)+'AC-'+id)
         sock.sendall(gen_message(5, gen_bitfield(meta)))
+        print gen_bitfield(meta)
         print "bitfield sent"
         while True:
                 resp = sock.recv(512)
                 if resp: print resp
                 
+                
 
 
 
         
-        
+
+
+
