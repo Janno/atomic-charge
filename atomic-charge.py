55e9346a8d036cd4de957cae209cde1318b2e4e5
Janno <mail@janno-kaiser.de>
2009-05-23 01:37:52 +0200

    midnight commit

--- atomic-charge.py
+++ atomic-charge.py
@@ -1,18 +1,37 @@
 #!/usr/bin/python 
-import bencode
+
 from hashlib import sha1
 from socket import socket
 from string import printable
 import struct
 import math
 import random
+import bencode
+
+def parse(msg):
+    """
+    returns (yieldwise): (id, msg) # :D
+    """
+    while msg:
+        # "..PWP messages are encoded as a 4-byte big-endian number"
+        assert len(msg) >= 4
+        msglen = struct.unpack('>I', msg[:4])
+        if msglen == 0:
+            # keep-alive, len 0
+            yield (0, '')
+            msg = msg[4:]
+        else:
+            msgid = msg[4]
+            yield (msgid, msg[4:4+msglen])
+            msg = msg[4+msglen:]
 
 def gen_handshake(meta):
     infohash = sha1(bencode.bencode(meta['info'])).digest()
-    return chr(19) + 'BitTorrent protocol' + chr(0)*8 + infohash
+    return chr(19) + 'BitTorrent protocol' + chr(0)*8 + infohash #XXX why \19?
 
 def gen_bitfield(meta):
-    pieces = len(meta['info']['pieces']) / 20
+    #XXX needs documentation
+    pieces = len(meta['info']['pieces']) / 20 #XXX why 20?
     num = int(math.ceil(pieces / 8.0))
     result = list('\xFF'*num)
     if pieces % 8:
@@ -23,8 +42,9 @@ def gen_bitfield(meta):
     return ''.join(result)
 
 def gen_message(msgid, msg = ''):
-    if msgid == 0: 
-        return '\x00'*4
+    if msgid == 0:
+        # keep-alive, message length = 0
+        return '\x00' * 4
     else:
         return struct.pack('>IB', len(msg)+1, msgid) + msg
     
