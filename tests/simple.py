730e3eebd09cd291394b484c561b75f8121d0884
Robert Lehmann <mail@robertlehmann.de>
2010-12-28 08:08:55 +0100

    Made tests generically runnable.

--- tests/simple.py
+++ tests/simple.py
@@ -1,18 +1,28 @@
-import atomic_charge
-import libtorrent
 import unittest
+import atomic_charge
+
+try:
+    import libtorrent
+except ImportError:
+    raise ImportError("please install python-libtorrent-rasterbar")
+
 import thread
 import time
 import os
+import tempfile
 
 class SingleFileTest(unittest.TestCase):
+    TORRENT = "single.torrent"
+    @property
+    def torrent(self):
+        return os.path.join(os.path.dirname(__file__), self.TORRENT)
     def setUp(self):
         # need to keep reference to session() to prevent GC picking it up
         self.session = session = libtorrent.session()
         session.listen_on(6881, 6882)
-        torrent = libtorrent.bdecode(open("single.torrent", 'rb').read())
+        torrent = libtorrent.bdecode(open(self.torrent, 'rb').read())
         info = libtorrent.torrent_info(torrent)
-        self.transfer = session.add_torrent(info, "/tmp")
+        self.transfer = session.add_torrent(info, tempfile.gettempdir())
 
     def test_transfer(self):
         thread.start_new_thread(self.charge, ())
@@ -20,24 +30,17 @@ class SingleFileTest(unittest.TestCase):
         assert self.transfer.is_seed() \
             or self.transfer.status().state in (3, 4, 5)
                                 # downloading, finished, or seeding
+        self.transfer.pause()
 
     def charge(self):
-        charger = atomic_charge.Charger("single.torrent", "./seed/lorem.txt",
+        charger = atomic_charge.Charger(self.torrent,
+                os.path.join(os.path.dirname(__file__), "seed", "lorem.txt"),
                                         "localhost", 6881)
         charger.begin()
 
     def tearDown(self):
-        try:
-            os.remove("/tmp/lorem.txt")
-        except OSError:
-            pass # we did not finish at all?
+        for entry in self.transfer.get_torrent_info().files():
+            os.remove(os.path.join(tempfile.gettempdir(), entry.path))
 
-
-#states = ('queued', 'checking', 'downloading metadata',
-#          'downloading', 'finished', 'seeding', 'allocating')
-#while not transfer.is_seed():
-#        s = transfer.status()
-#        print '%.2f%%, % 2d peers (%s)' % (
-#            s.progress * 100, s.num_peers, states[s.state])
 if __name__ == '__main__':
     unittest.main()
