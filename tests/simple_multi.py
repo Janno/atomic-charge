75bb07c1733b46962893980e79f4cbc049a09dc6
Jan-Oliver Kaiser <mail@janno-kaiser.de>
2011-01-01 14:21:24 +0100

    multi-piece and multi-file torrent tests

--- tests/simple_multi.py
+++ tests/simple_multi.py
@@ -11,15 +11,16 @@ import time
 import os
 import tempfile
 
-class SingleFileTest(unittest.TestCase):
-    TORRENT = "single.torrent"
+
+class MultiFileTest(unittest.TestCase):
+    TORRENT = "multi.torrent"
     @property
     def torrent(self):
         return os.path.join(os.path.dirname(__file__), self.TORRENT)
     def setUp(self):
         # need to keep reference to session() to prevent GC picking it up
         self.session = session = libtorrent.session()
-        session.listen_on(6881, 6882)
+        session.listen_on(6885, 6886)
         torrent = libtorrent.bdecode(open(self.torrent, 'rb').read())
         info = libtorrent.torrent_info(torrent)
         self.transfer = session.add_torrent(info, tempfile.gettempdir())
@@ -34,8 +35,8 @@ class SingleFileTest(unittest.TestCase):
 
     def charge(self):
         charger = atomic_charge.Charger(self.torrent,
-                os.path.join(os.path.dirname(__file__), "seed", "lorem.txt"),
-                                        "localhost", 6881)
+                os.path.join(os.path.dirname(__file__), "seed"),
+                                        "localhost", 6885)
         charger.begin()
 
     def tearDown(self):
